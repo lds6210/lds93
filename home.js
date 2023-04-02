4dbd36a677d140607cd8854f7c994945bad61945
Lee <dongsub.lee@medistream.co.kr>
2023-04-02 23:47:52 +0900

    spaGPT

--- home.js
+++ home.js
@@ -1,28 +1,29 @@
 const gameButton = document.querySelector("#game-button");
 const chatButton = document.querySelector("#chat-button");
 const pictureButton = document.querySelector("#picture-button");
+const spaGPTButton = document.querySelector("#spaGPT-button");
 
 // gameButton.textContent = "게임";
 // chatButton.textContent = "Chat GPT";
 // pictureButton.textContent = "인생네컷";
 
 gameButton.addEventListener("click", () => {
-  gameButton.style.backgroundColor = "#ff9800";
   gameButton.textContent = "로딩 중...";
   // 페이지 이동
   location.href = "./gameList/index.html";
 });
 
-
-
 chatButton.addEventListener("click", () => {
-  chatButton.style.backgroundColor = "#9c27b0";
   chatButton.textContent = "로딩 중...";
   location.href = "./chatgpt/index.html";
 });
 
 pictureButton.addEventListener("click", () => {
-  chatButton.style.backgroundColor = "#4c2800";
   chatButton.textContent = "로딩 중...";
   location.href = "./pictureOfLife/index.html";
+});
+
+spaGPTButton.addEventListener("click", () => {
+  chatButton.textContent = "로딩 중...";
+  location.href = "./spaGPT/index.html";
 });
