b0b83b39ed89db92844cc33de0714cd99a43a248
Lee <dongsub.lee@medistream.co.kr>
2023-04-02 19:21:09 +0900

    파일 이름 변경

--- home.js
+++ home.js
@@ -2,12 +2,18 @@ const gameButton = document.querySelector("#game-button");
 const chatButton = document.querySelector("#chat-button");
 const pictureButton = document.querySelector("#picture-button");
 
+// gameButton.textContent = "게임";
+// chatButton.textContent = "Chat GPT";
+// pictureButton.textContent = "인생네컷";
+
 gameButton.addEventListener("click", () => {
   gameButton.style.backgroundColor = "#ff9800";
   gameButton.textContent = "로딩 중...";
   // 페이지 이동
   location.href = "./gameList/index.html";
 });
+
+
 
 chatButton.addEventListener("click", () => {
   chatButton.style.backgroundColor = "#9c27b0";
