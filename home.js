f30794c621d19e1ece04a83287c940119500f56a
Lee <dongsub.lee@medistream.co.kr>
2023-05-07 22:39:57 +0900

    test

--- home.js
+++ home.js
@@ -2,14 +2,16 @@ const gameButton = document.querySelector("#game-button");
 const chatButton = document.querySelector("#chat-button");
 const pictureButton = document.querySelector("#picture-button");
 const spaGPTButton = document.querySelector("#spaGPT-button");
+const fastcamButton = document.querySelector("#fastcam-button");
 
-// gameButton.textContent = "게임";
-// chatButton.textContent = "Chat GPT";
-// pictureButton.textContent = "인생네컷";
+gameButton.textContent = "게임";
+chatButton.textContent = "chatGPT";
+pictureButton.textContent = "인생네컷";
+spaGPTButton.textContent = "spaGPT";
+fastcamButton.textContent = "소셜로그인";
 
 gameButton.addEventListener("click", () => {
   gameButton.textContent = "로딩 중...";
-  // 페이지 이동
   location.href = "./gameList/index.html";
 });
 
@@ -19,11 +21,16 @@ chatButton.addEventListener("click", () => {
 });
 
 pictureButton.addEventListener("click", () => {
-  chatButton.textContent = "로딩 중...";
+  pictureButton.textContent = "로딩 중...";
   location.href = "./pictureOfLife/index.html";
 });
 
 spaGPTButton.addEventListener("click", () => {
-  chatButton.textContent = "로딩 중...";
+  spaGPTButton.textContent = "로딩 중...";
   location.href = "./spaGPT/index.html";
+});
+
+fastcamButton.addEventListener("click", () => {
+  fastcamButton.textContent = "로딩 중...";
+  location.href = "./fastcam/index.html";
 });
